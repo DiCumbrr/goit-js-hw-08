6eb00ff2b9c922992bdd7bb2a6546dd055309885
diana <dianakondratchuk@gmail.com>
2022-10-21 17:16:52 +0300

    updated ex-02 & ex-03

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,31 +1,32 @@
-const iframe = document.querySelector('iframe')
-const player = new Vimeo.Player(iframe)
-const throttle = require('lodash.throttle')
+import Player from'@vimeo/player';
+import throttle from 'lodash.throttle';
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title)
-})
+const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+    const SAVE_PLAY = "videoplayer-current-time";
+    const onPlay = function (data) {
+        localStorage.setItem(SAVE_PLAY, JSON.stringify(data['seconds']));
+    };
+player.on('timeupdate', throttle(onPlay, 1000));
 
-player.on('timeupdate', throttle(setTime, 1000))
 
-function setTime(data) {
-  time = data.seconds
-  localStorage.setItem('videoplayer-current-time', time)
-}
-
-player
-  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break
-
-      default:
-        // some other error occurred
-        break
+function resumePlayback() {
+    if (JSON.parse(localStorage.getItem(SAVE_PLAY)) === null) {
+        return;
     }
-  });
+    const time = JSON.parse(localStorage.getItem(SAVE_PLAY));
+    if (time) {
+        player
+        .setCurrentTime(time)
+        .then(function (seconds) {})
+        .catch(function (error) {
+            switch (error.name) {
+                case 'RangeError':
+                    break;
+                default:
+                    break;
+            }
+        });
+    }
+}
+resumePlayback(); 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,37 +1,41 @@
-const throttle = require('lodash.throttle')
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
 const refs = {
-  form: document.querySelector('.feedback-form'),
-  texarea: document.querySelector('textarea')
+    form: document.querySelector('.feedback-form'),
+    textarea: document.querySelector('.feedback-form textarea'),
+    input: document.querySelector('input'),
+};
+let formData = {};
+
+refs.form.addEventListener('input', throttle(onTextareaInput, 500));
+
+refs.form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    localStorage.removeItem(STORAGE_KEY);
+    formData = {};
+    const { elements: { email, message } } = event.currentTarget;
+    if (email.value === '' || message.value === '') {
+        return alert('Заполните все поля!')
+    }
+
+    console.log({email: email.value, message: message.value});
+    refs.form.reset();
+});
+
+function onTextareaInput(event) {
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
-const MESSEGE_KEY = 'messege'
-messegeToTextarea()
-
-const formData = {}
-
-refs.form.addEventListener('submit', onFormSubmit)
-refs.texarea.addEventListener('input', throttle(onTextareaInput, 500))
-
-refs.form.addEventListener('input', e => {
-  formData[e.target.name] = e.target.value
-})
-
-function onFormSubmit(evt) {
-  evt.preventDefault()
-  evt.currentTarget.reset()
-  localStorage.removeItem(MESSEGE_KEY)
-  console.log(formData)
+function populateTextarea() {
+    const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if (savedMessage === null) {
+        return;
+    }
+    refs.input.value = savedMessage.email || '';
+    refs.textarea.value = savedMessage.message || '';
 }
 
-function onTextareaInput(evn) {
-  const massege = evn.target.value
-  localStorage.setItem(MESSEGE_KEY, massege)
-}
-
-function messegeToTextarea() {
-  const sevedMessege = localStorage.getItem(MESSEGE_KEY)
-  if (sevedMessege) {
-    refs.texarea.value = sevedMessege
-  }
-}
+populateTextarea();
